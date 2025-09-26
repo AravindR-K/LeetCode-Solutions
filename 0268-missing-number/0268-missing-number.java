c5b4a30e449be6312f3a5276dbcd080eaf2d11af
Aravind RK <146981557+AravindR-K@users.noreply.github.com>
2025-09-26 18:10:58 +0530

    Time: 0 ms (100%), Space: 45.8 MB (13.53%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -2,16 +2,11 @@ import java.util.Arrays;
 class Solution {
     public int missingNumber(int[] nums) {
         int n = nums.length;
-        int hashArr[] = new int[n+1];
+        int sum = (n * (n+1))/2;
+        int sumNum = 0;
         for (int i=0; i<n; i++) {
-            hashArr[nums[i]]++;
+            sumNum += nums[i];
         }
-        System.out.println(Arrays.toString(hashArr));
-        for (int i=0; i<n+1; i++) {
-            if (hashArr[i] == 0) {
-                return i;
-            }
-        }
-        return -1;
+        return sum - sumNum; 
     }
 }
