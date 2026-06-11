cf90f7da040e952a8166647b3aab4ba776cd27f1
Aravind RK <146981557+AravindR-K@users.noreply.github.com>
2026-06-11 10:18:18 +0530

    Time: 0 ms (100%), Space: 42.2 MB (15.84%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,13 +1,16 @@
 class Solution {
+    public int f(int n, int[] dp) {
+        if (n <= 1) {
+            return n;
+        }
+        if (dp[n] != -1) return dp[n];
+        dp[n] = f(n-1, dp) + f(n-2, dp);
+        return dp[n];
+    }
+
     public int fib(int n) {
-        if (n <= 0) {
-            return 0;
-        }
-        if (n == 1) {
-            return 1;
-        }
-        int prev = fib(n-1);
-        int secPrev = fib(n-2);
-        return prev + secPrev;
+        int[] dp = new int[n+1];
+        Arrays.fill(dp, -1);
+        return f(n, dp);
     }
 }
