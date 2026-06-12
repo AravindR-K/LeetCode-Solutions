f3dd0de3b58368c892aedf9c48f14f08ca0eef5b
Aravind RK <146981557+AravindR-K@users.noreply.github.com>
2026-06-12 14:56:02 +0530

    Time: 0 ms (100%), Space: 41.9 MB (85.36%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,16 +1,14 @@
 class Solution {
-    public int climbStairs(int n, Map<Integer, Integer> memo) {
-        if (n == 0 || n == 1) {
+    public int f(int n, int[] dp) {
+        if (n <= 1) {
             return 1;
         }
-        if (!memo.containsKey(n)) {
-            memo.put(n, climbStairs(n-1,memo) + climbStairs(n-2, memo));
-        }
-        return memo.get(n);
+        if (dp[n] != -1) return dp[n];
+        return dp[n] = f(n-1, dp) + f(n-2, dp);
     }
-
     public int climbStairs(int n) {
-        Map<Integer, Integer> memo = new HashMap<>();
-        return climbStairs(n, memo);
+        int[] dp = new int[n+1];
+        Arrays.fill(dp, -1);
+        return f(n, dp);
     }
 }
