ce0f5dcb0d9136485d73bea4ded21ba91f9500ea
Aravind RK <146981557+AravindR-K@users.noreply.github.com>
2026-08-05 17:36:14 +0530

    Time: 0 ms (100%), Space: 42.8 MB (61.3%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,32 +1,32 @@
 class Solution {
 
-    public int f(int[] coins) {
-        int n = coins.length;
-        int prev = coins[0];
-        int prev2 = 0;
-        for (int i=1; i<n; i++) {
-            int take = coins[i];
-            if (i>1) take += prev2;
-            int notTake = 0 + prev;
-            int curr = Math.max(take, notTake);
-            prev2 = prev;
-            prev = curr; 
-        }
-        return prev;
-    }
+    public int fFirst(int[] nums, int n, int[] dp) {
+        if (n == 0) return nums[0];
+        if (n < 0) return 0;
+        if (dp[n] != -1) return dp[n];
+        int pick = nums[n] + fFirst(nums, n-2, dp);
+        int notPick = 0 + fFirst(nums, n-1, dp);
+        return dp[n] = Math.max(pick, notPick);
+    } 
+
+    public int fEnd(int[] nums, int n, int[] dp) {
+        if (n == 1) return nums[1];
+        if (n < 1) return 0;
+        if (dp[n] != -1) return dp[n];
+        int pick = nums[n] + fEnd(nums, n-2, dp);
+        int notPick = 0 + fEnd(nums, n-1, dp);
+        return dp[n] = Math.max(pick, notPick);
+    } 
 
     public int rob(int[] nums) {
         int n = nums.length;
-        if (n==1) return nums[0];
-        int[] temp1 = new int[n-1];
-        int[] temp2 = new int[n-1];
-        int count = 0;
-        for (int i=0; i<n; i++) {
-            if (i != 0) temp1[i-1] = nums[i];
-            if (i != n-1) temp2[i] = nums[i];
-            count++;
+        if (n == 1) {
+            return nums[0];
         }
-
-        return Math.max(f(temp1), f(temp2));
+        int[] dp1 = new int[n];
+        int[] dp2 = new int[n];
+        Arrays.fill(dp1, -1);
+        Arrays.fill(dp2, -1);
+        return Math.max(fEnd(nums, n-1, dp1), fFirst(nums, n-2, dp2));
     }
 }
